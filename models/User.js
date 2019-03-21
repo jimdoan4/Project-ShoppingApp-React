b5783371da7fd7a18fc380ba43af616ab813d2e1
Jim Doan <jimdoan4@gmail.com>
2019-03-21 11:35:02 -0400

    adding crud to user controller

--- models/User.js
+++ models/User.js
@@ -1,14 +1,28 @@
 const mongoose = require('../db/connection.js');
 const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
 const UserSchema = new Schema({
-	firstName: String,
-	lastName: String,
-	userName: String,
-	password: String,
-	email: String,
-	age: Number,
-	gender: String
+	first_name: {
+		type: String
+	},
+	last_name: {
+		type: String
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 module.exports = mongoose.model('User', UserSchema);
+
+// module.exports = User = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-const express = require('express')
-const logger = require('morgan')
-const app = express()
-const routes = require('./routes/index.js')
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const routes = require('./routes/index.js');
+const PORT = process.env.PORT || 3001;
 
-# these are middlewares
-app.use(logger('dev'))
+// # these are middlewares
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/build`));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 
-app.use('/api/v1', routes)
-
+app.use('/api/v1', routes);
 app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
-})
+	res.sendFile(`${__dirname}/client/build/index.html`);
+});
 
-const PORT = process.env.PORT || 3001
+const mongoURI = (MONGODB_URI = 'mongodb://localhost/shopping-app');
+
+mongoose
+	.connect(mongoURI, { useNewUrlParser: true })
+	.then(() => console.log('MongoDb is connected'))
+	.catch((err) => console.log(err));
+
+// const Users = require('./routes/Users');
+
+// app.use('/users', Users);
 
 app.listen(PORT, () => {
-  console.log('App is up and running on port ' + PORT)
-})
+	console.log('App is up and running on port ' + PORT);
+});
