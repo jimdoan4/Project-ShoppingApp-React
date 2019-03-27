9dec4a7209ff9da5b644fdf397127fe96ebeccac
Jim Doan <jimdoan4@gmail.com>
2019-03-26 23:12:43 -0400

    seeding heroku

--- db/connections.js
+++ db/connections.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
-if (process.env.MONGODB_URI) {
-	mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-} else {
-	mongoose.connect('mongodb://localhost/shopping-app');
-}
+// if (process.env.MONGODB_URI) {
+// 	mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+// } else {
+// 	mongoose.connect('mongodb://localhost/shopping-app');
+// }
 
 mongoose.connection.once('open', () => {
 	console.log(`Mongoose has connected to MongoDB`);
