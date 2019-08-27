50cf76e6c24668d830d4473bb7af12fa398543b1
Jim Doan <jimdoan4@gmail.com>
2019-08-27 15:05:09 -0400

    made changes to navbar and images

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,69 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { Nav } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import { NavDropdown } from 'react-bootstrap';
-import MenClothes from './components/MenClothes';
-import Home from './components/Home';
-import UserAccount from './components/UserAccount';
-import UserPage from './components/UserPage';
-import ClothesPage from './components/ClothesPage';
-import SingleReview from './components/SingleReview';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
+import MenClothes from "./components/MenClothes";
+import Home from "./components/Home";
+import UserAccount from "./components/UserAccount";
+import UserPage from "./components/UserPage";
+import ClothesPage from "./components/ClothesPage";
+import SingleReview from "./components/SingleReview";
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div>
-					<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-						<Navbar.Brand href="/">EQUIS</Navbar.Brand>
-						<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-						<Navbar.Collapse id="responsive-navbar-nav">
-							<Nav className="mr-auto">
-								<NavDropdown title="Shop" id="collasible-nav-dropdown">
-									<NavDropdown.Item>
-										<Link to="/apparels/" style={{ color: 'black ' }}>
-											Men Apparel
-										</Link>
-									</NavDropdown.Item>
-									
-								</NavDropdown>
-							</Nav>
-							<Nav.Link>
-								<Link to="/users/" style={{ color: 'white ' }}>
-									User Account
-								</Link>
-							</Nav.Link>
-							
-						</Navbar.Collapse>
-					</Navbar>
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar className="navbar" collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/">EQUIS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <NavDropdown title="Shop" id="collasible-nav-dropdown">
+                  <NavDropdown.Item>
+                    <Link to="/apparels/" style={{ color: "black " }}>
+                      Men Apparel
+                    </Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <div class="iconic">
+                <i
+                  class="fa fa-search"
+                ></i>
+                <i
+                  class="fa fa-shopping-cart"
+                ></i>
+                <i
+                  class="fa fa-heart-o"
+                ></i>
+                <Link to="/users/" style={{ color: "white " }}>
+                  <i
+                    class="fa fa-male"
+                  ></i>
+                </Link>
+              </div>
+            </Navbar.Collapse>
+          </Navbar>
 
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/apparels/" component={MenClothes} />
-						<Route exact path="/users/" component={UserAccount} />
-						<Route exact path="/users/:userId/" component={UserPage} />
-						<Route exact path="/apparels/:apparelId/" component={ClothesPage} />
-						<Route exact path="/apparels/:apparelId/reviews/:reviewId/" component={SingleReview} />
-					</Switch>
-				</div>
-			</Router>
-		);
-	}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/apparels/" component={MenClothes} />
+            <Route exact path="/users/" component={UserAccount} />
+            <Route exact path="/users/:userId/" component={UserPage} />
+            <Route exact path="/apparels/:apparelId/" component={ClothesPage} />
+            <Route
+              exact
+              path="/apparels/:apparelId/reviews/:reviewId/"
+              component={SingleReview}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import { Jumbotron } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
 import { CardGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -10,219 +9,62 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div>
-				<Jumbotron fluid className="home" style={{ height: '26rem' }}>
-					<Container className="homefont">
-						<h1 style={{ fontSize: '42px', fontWeight: 'bold', color: '#6a7587' }}>EQUIS Men Apparel</h1>
-						<p style={{ fontSize: '20px', color: '#495260' }}>Fitness Wear that hone Your MIND, BODY and SOUL</p>
+				<Jumbotron className="home" style={{ height: '26rem' }}>
+					<Container className="main-header-img intro-header">
+						<h1>EQUIS Men Apparel</h1>
 					</Container>
 				</Jumbotron>
-				<div style={{ marginLeft: '30px', marginRight: '30px', marginBottom: '30px', height: '20rem' }}>
-					<CardGroup>
-						<Card style={{ width: '18rem', marginLeft: '30px', marginTop: '14px', marginRight: '30px' }}>
-							<Card.Img
-								variant="top"
-								src="https://images.lululemon.com/is/image/lululemon/LM3BF7S_038068_1?$image_carousel-lg$"
-								alt="top"
-							/>
-						</Card>
+				<div id="event">
+        <div class="container-fluid text-center">
+<div class="container-fluid">
+            <div class="os-animation" data-animation="fadeInUp">
+              <div class="row text-center"> 
+                <div class="col main-img-intro">
+                      <img class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LM3BF7S_038068_1?$image_carousel-lg$"/>
+                </div>
+  
+                <div class="col main-img-intro">
+                      <img class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LM3AUTS_037049_1?$image_carousel-lg$"/>
+                </div>
+            </div>
+          </div> 
+      </div>
 
-						<Card style={{ width: '18rem', marginTop: '14px', marginLeft: '30px', marginRight: '30px' }}>
-							<Card.Img
-								variant="top"
-								src="https://images.lululemon.com/is/image/lululemon/LM3AUTS_037049_1?$image_carousel-lg$"
-								alt="top"
-							/>
-						</Card>
-					</CardGroup>
-					<Container>
-						<h5
-							className="homefont"
-							style={{
-								marginTop: '110px',
-								marginBottom: '110px',
-								marginLeft: '70px',
-								marginRight: '70px',
-								color: '#495260'
-							}}
-						>
-							EQUIS, the haven to your fitness clothing guides. Browse through our apparel and find the best fitwear for your daily Lifestyle.
-						</h5>
-					</Container>
+					<div id="event">
 					<div>
-						<Jumbotron fluid className="homes" style={{ height: '26rem', marginTop: '20px' }}>
-							<Container style={{ fontWeight: 'bold', color: '#495260' }}>
-								<h1
-									className="fonts"
-									style={{ fontWeight: 'bold', color: 'white', marginTop: '170px' }}
-								>
-									Women Fitness Wear - Coming Soon!
-								</h1>
-							</Container>
-						</Jumbotron>
-					</div>
-					<div
-						style={{
-							marginLeft: '16px',
-							marginRight: '16px',
-							marginBottom: '30px',
-							marginTop: '45px'
-						}}
-					>
-						<CardGroup>
-							<Card style={{ width: '23rem', marginLeft: '10px', marginRight: '10px' }}>
-								<Card.Img
-									variant="top"
-									src="https://cdn-us-ec.yottaa.net/5b9fc7760b534475fab84ce9/www.gnc.com/v~4b.1e/dw/image/v2/BBLB_PRD/on/demandware.static/-/Sites-master-catalog-gnc/default/dw65b2a723/hi-res/217908_1.jpg?sw=305&sh=305&sm=fit&yocs=s_f_&yoloc=us"
-									alt="top"
-								/>
-
-								<Card.Body>
-									<Card.Title>City Sweat Pullover Hoodie</Card.Title>
-									<Card.Title>$68.00</Card.Title>
-								</Card.Body>
-							</Card>
-							<Card style={{ width: '23rem', marginLeft: '30px', marginRight: '30px' }}>
-								<Card.Img
-									variant="top"
-									src="https://cdn-us-ec.yottaa.net/5b9fc7760b534475fab84ce9/www.gnc.com/v~4b.1e/dw/image/v2/BBLB_PRD/on/demandware.static/-/Sites-master-catalog-gnc/default/dw892b22b7/hi-res/253034_1.jpg?sw=305&sh=305&sm=fit&yocs=s_f_&yoloc=us"
-									alt="top"
-								/>
-								<Card.Body>
-									<Card.Title>City Sweat Zip Hoodie</Card.Title>
-									<Card.Title>$128.00</Card.Title>
-								</Card.Body>
-							</Card>
-							<Card style={{ width: '23rem', marginLeft: '30px', marginRight: '30px' }}>
-								<Card.Img
-									variant="top"
-									src="https://cdn-us-ec.yottaa.net/5b9fc7760b534475fab84ce9/www.gnc.com/v~4b.1e/dw/image/v2/BBLB_PRD/on/demandware.static/-/Sites-master-catalog-gnc/default/dw6aabec55/hi-res/520810_1.jpg?sw=305&sh=305&sm=fit&yocs=s_f_&yoloc=us"
-									alt="top"
-								/>
-								<Card.Body>
-									<Card.Title>Accedo Vest Hoodie</Card.Title>
-									<Card.Title>$78.00</Card.Title>
-								</Card.Body>
-							</Card>
-							<Card style={{ width: '23rem', marginLeft: '14px', marginRight: '14px' }}>
-								<Card.Img
-									variant="top"
-									src="https://cdn-us-ec.yottaa.net/5b9fc7760b534475fab84ce9/www.gnc.com/v~4b.1e/dw/image/v2/BBLB_PRD/on/demandware.static/-/Sites-master-catalog-gnc/default/dwd8ef5fc3/hi-res/386576_1.jpg?sw=305&sh=305&sm=fit&yocs=s_f_&yoloc=us"
-									alt="top"
-								/>
-								<Card.Body>
-									<Card.Title>Accedo Vest Hoodie</Card.Title>
-									<Card.Title>$78.00</Card.Title>
-								</Card.Body>
-							</Card>
-						</CardGroup>
-					</div>
-					<Container style={{ textAlign: 'center' }}>
-						<h1
-							className="homefont"
-							style={{
-								marginTop: '80px',
-								marginBottom: '80px',
-								marginLeft: '70px',
-								marginRight: '70px',
-								color: '#495260'
-							}}
-						>
-							Check out our Accessories - Coming Soon!
-						</h1>
-					</Container>
-
-					<div style={{ marginTop: '40px', background: 'cover' }}>
-						<Jumbotron fluid className="homee" style={{ width: 'max', height: '26rem' }}>
-							<Container>
-								<h1 className="fonts" style={{ fontWeight: 'bold', color: 'white', marginTop: '70px' }}>
-									Fitness Accessories - Coming Soon!
-								</h1>
-							</Container>
-						</Jumbotron>
-					</div>
-					<div style={{ marginLeft: '10px', marginRight: '10px' }}>
-						<CardGroup style={{ marginTop: '50px' }}>
-							<Card style={{ width: '18rem', marginLeft: '2px', marginRight: '2px' }}>
-								<Card.Img
-									variant="top"
-									src="https://images.lululemon.com/is/image/lululemon/LU9A73S_037040_1?$image_carousel-lg_2x$"
-									alt="top"
-								/>
-
-								<Card.Body>
-									<Card.Title>The Reversible Mat 5mm</Card.Title>
-									<Card.Title>$78.00</Card.Title>
-								</Card.Body>
-							</Card>
-							<Card style={{ width: '18rem', marginLeft: '2px', marginRight: '2px' }}>
-								<Card.Img
-									variant="top"
-									src="https://images.lululemon.com/is/image/lululemon/LM9767S_036763_2?$product_tile-lg_2x$"
-									alt="top"
-								/>
-
-								<Card.Body>
-									<Card.Title>Command BackPack</Card.Title>
-									<Card.Title>$148.00</Card.Title>
-								</Card.Body>
-							</Card>
-							<Card style={{ width: '18rem', marginLeft: '2px', marginRight: '2px' }}>
-								<Card.Img
-									variant="top"
-									src="https://images.lululemon.com/is/image/lululemon/LM9723S_034135_2?$product_tile-lg_2x$"
-									alt="top"
-								/>
-
-								<Card.Body>
-									<Card.Title>Surge BackPack</Card.Title>
-									<Card.Title>$168.00</Card.Title>
-								</Card.Body>
-							</Card>
-						</CardGroup>
-
-						<div style={{ marginTop: '20px', marginBottom: '30px' }}>
-							<CardGroup>
-								<Card style={{ width: '18rem', marginLeft: '2px', marginRight: '2px' }}>
-									<Card.Img
-										variant="top"
-										src="https://images.lululemon.com/is/image/lululemon/LM9474S_037617_2?$product_tile-lg_2x$"
-										alt="top"
-									/>
-
-									<Card.Body>
-										<Card.Title>Lightspeed Hat</Card.Title>
-										<Card.Title>$38.00</Card.Title>
-									</Card.Body>
-								</Card>
-								<Card style={{ width: '18rem', marginLeft: '2px', marginRight: '2px' }}>
-									<Card.Img
-										variant="top"
-										src="https://images.lululemon.com/is/image/lululemon/LM9641S_031382_2?$product_tile-lg_2x$"
-										alt="top"
-									/>
-
-									<Card.Body>
-										<Card.Title>Cold Beanie</Card.Title>
-										<Card.Title>$28.00</Card.Title>
-									</Card.Body>
-								</Card>
-								<Card style={{ width: '18rem', marginLeft: '2px', marginRight: '2px' }}>
-									<Card.Img
-										variant="top"
-										src="https://images.lululemon.com/is/image/lululemon/LU9AF5S_0001_2?$product_tile-lg_2x$"
-										alt="top"
-									/>
-
-									<Card.Body>
-										<Card.Title>Stay Cold Bottle</Card.Title>
-										<Card.Title>$48.00</Card.Title>
-									</Card.Body>
-								</Card>
-							</CardGroup>
-						</div>
-					</div>
+                <button class="btn btn-custom" type="submit" value="Send">SHOP WHAT'S NEW</button>
+              </div>
+		<div class="container-fluid">
+              <div class="row text-center">
+                <div class="col secondary-img-intro">
+                      <img class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LU9A73S_037040_1?$image_carousel-lg_2x$"/> 
+                </div>
+				<div class="col secondary-img-intro">                 
+                      <img style={{width: '20rem', height: '460px'}} class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LM9767S_036763_2?$product_tile-lg_2x$"/>          
+                </div>
+				<div class="col secondary-img-intro">
+                      <img style={{width: '20rem', height: '460px'}} class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LM5609S_1971_1?$image_carousel-lg_2x$"/>
+                </div>
 				</div>
+				<br/>
+				<div class="row text-center">
+				<div class="col secondary-img-intro">
+                      <img class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LM9474S_037617_2?$product_tile-lg_2x$"/>
+                </div>
+				<div class="col secondary-img-intro">
+                      <img class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LM9641S_031382_2?$product_tile-lg_2x$"/>
+                </div>
+                <div class="col secondary-img-intro">
+                      <img class="card-img-top" src="https://images.lululemon.com/is/image/lululemon/LU9AF5S_0001_2?$product_tile-lg_2x$"/>
+                </div>
+            </div>
 			</div>
+			</div>
+			</div>
+			</div>
+			</div>
+	
+
 		);
 	}
 }
