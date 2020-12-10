b484badfa6e699c406d72d84142f8db89a38a74f
Jim Doan <jimdoan@icloud.com>
2020-12-10 14:15:30 -0500

    d

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,21 @@ const mongoose = require('mongoose');
 if(process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI);
 } else {
-    mongoose.connect('mongodb://localhost/shopping-application', {useNewUrlParser: true, useUnifiedTopology: true})
-}
+    mongoose.connect('mongodb://localhost/shopping-application',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      dbName: 'shopping-application', // IMPORTANT TO HAVE IT HERE AND NOT IN CONNECTION STRING
+    },
+    err => { throw err; },
+  )};
+
+		
+		
+// 		'mongodb://localhost/shopping-application', {useNewUrlParser: true, useUnifiedTopology: true})
+// }
 
 mongoose.connection.once('open', () => {
 	console.log(`Mongoose has connected to MongoDB`);
