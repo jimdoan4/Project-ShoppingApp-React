e8bcbca40acc84ea6da3ecf8bc8e0a39f4f931bc
Jim Doan <jimdoan@icloud.com>
2020-12-10 15:15:06 -0500

    d

--- db/connection.js
+++ db/connection.js
@@ -6,17 +6,16 @@ if(process.env.MONGODB_URI) {
 } else {
     mongoose.connect('mongodb://localhost/shopping-application', {useNewUrlParser: true, useUnifiedTopology: true})
 }
-
-mongoose.connection.on('error', (err) => {
-    console.error('MongoDB connection error: ', err)
-    process.exit(-1)
-})
-
 mongoose.connection.once('open', () => {
-    console.log("Mongoose has connected to MongoDB")
-})
+	console.log(`Mongoose has connected to MongoDB`);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', (error) => {
+	console.error(`MongoDB connection error!!! ${error}`);
+	process.exit(-1);
+});
 //export here
-module.exports = mongoose
 		
 		
 // 		'mongodb://localhost/shopping-application', {useNewUrlParser: true, useUnifiedTopology: true})
